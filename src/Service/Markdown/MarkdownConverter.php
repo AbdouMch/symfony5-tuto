ba92c0625075cbd9849dd5cdc398861940d184e0
Abderrahmen.Mchayaa <abderrahmen.mchayaa@gmail.com>
2022-09-12 08:14:39 +0100

    container parameters and environments

--- src/Service/Markdown/MarkdownConverter.php
+++ src/Service/Markdown/MarkdownConverter.php
@@ -6,23 +6,27 @@ use League\CommonMark\CommonMarkConverter;
 use League\CommonMark\ConverterInterface;
 use League\CommonMark\Output\RenderedContentInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class MarkdownConverter implements MarkdownConverterInterface
 {
     private ConverterInterface $converter;
     private CacheInterface $cache;
+    private int $cacheTTL;
 
-    public function __construct(CacheInterface $cache)
+    public function __construct(CacheInterface $cache, int $cacheTTL)
     {
         $this->converter = new CommonMarkConverter();
         $this->cache = $cache;
+        $this->cacheTTL = $cacheTTL;
     }
 
     public function convert(string $input): RenderedContentInterface
     {
         $cacheKey = $this->getCacheKey($input);
 
-        return $this->cache->get($cacheKey, function () use ($input) {
+        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($input) {
+            $item->expiresAfter($this->cacheTTL);
             return $this->converter->convert($input);
         });
     }
