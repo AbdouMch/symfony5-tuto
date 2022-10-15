571a0c82b32000611e1c3a97afb13e2aef30e5df
Abderrahmen.Mchayaa <abderrahmen.mchayaa@gmail.com>
2022-10-15 13:43:15 +0100

    cs-fix

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -20,7 +20,6 @@ class Question
 
     /**
      * @ORM\Column(type="string", length=255)
-     *
      * @Assert\NotBlank(message="question.title.not_blank")
      * @Assert\Length(min=10, minMessage="question.title.min_length")
      */
@@ -28,7 +27,6 @@ class Question
 
     /**
      * @ORM\Column(type="text")
-     *
      * @Assert\NotBlank(message="question.content.not_blank")
      * @Assert\Length(min=100, minMessage="question.content.min_length")
      */
--- src/Form/QuestionFormType.php
+++ src/Form/QuestionFormType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class QuestionFormType extends AbstractType
